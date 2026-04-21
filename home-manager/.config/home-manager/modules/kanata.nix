8eca8eb8e53f7fa3ee19b79f084435a8ab7cd2e1
Henning Dahlheim <dev@dahlheim.ch>
2026-04-21 17:32:23 +0200

    feat: add kanata config

---
+++ home-manager/.config/home-manager/modules/kanata.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  home.file.".config/kanata/kanata.kbd".text = ''
+    (defcfg
+      process-unmapped-keys yes
+    )
+
+    (defsrc
+      caps
+    )
+
+    (deflayer base
+      @cap
+    )
+
+    (defalias
+      cap (tap-hold 200 200 esc lctl)
+    )
+  '';
+
+  launchd.agents.kanata = {
+    enable = true;
+    config = {
+      Label = "org.local.kanata";
+      ProgramArguments = [
+        "${pkgs.kanata}/bin/kanata"
+        "--cfg"
+        "${config.home.homeDirectory}/.config/kanata/kanata.kbd"
+      ];
+      RunAtLoad = true;
+      KeepAlive = true;
+      StandardOutPath = "${config.home.homeDirectory}/Library/Logs/kanata.log";
+      StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/kanata-error.log";
+    };
+  };
+}
