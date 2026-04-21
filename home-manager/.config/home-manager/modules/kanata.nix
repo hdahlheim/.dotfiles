7c1fd1a1f6347ed02b0b81791e24c97d76dfa00b
Henning Dahlheim <dev@dahlheim.ch>
2026-04-21 17:45:16 +0200

    fix: remove launchd kanata setup

--- home-manager/.config/home-manager/modules/kanata.nix
+++ home-manager/.config/home-manager/modules/kanata.nix
@@ -18,20 +18,4 @@
       cap (tap-hold 200 200 esc lctl)
     )
   '';
-
-  launchd.agents.kanata = {
-    enable = true;
-    config = {
-      Label = "org.local.kanata";
-      ProgramArguments = [
-        "${config.home.homeDirectory}/.nix-profile/bin/kanata"
-        "--cfg"
-        "${config.home.homeDirectory}/.config/kanata/kanata.kbd"
-      ];
-      RunAtLoad = true;
-      KeepAlive = true;
-      StandardOutPath = "${config.home.homeDirectory}/Library/Logs/kanata.log";
-      StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/kanata-error.log";
-    };
-  };
 }
